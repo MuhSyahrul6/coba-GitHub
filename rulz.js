0d346d622a25bc2cc0cccd69d137e2aec25318f2
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-09-08 21:15:18 +0700

    Update rulz.js
    Co-Authored-By: SyahrulR6 <107108452+SyahrulR6@users.noreply.github.com>

--- rulz.js
+++ rulz.js
@@ -1,1 +1,7 @@
-Muhammad Syahrul Romadhon
+let kata1 = "Halo";
+let kata2 = "Dunia";
+let kalimat = kata1 + " " + kata2; // Menggunakan operator +
+console.log(kalimat);
+
+let kalimat2 = kata1.concat(" ", kata2); // Menggunakan concat() method
+console.log(kalimat2);
